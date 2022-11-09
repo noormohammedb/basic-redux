632c717995b979550556a9174c1dd4135ef7f623
Noor Mohammed B <noormohammedb@protonmail.com>
2022-11-09 12:27:55 +0400

    Basic counter
    redux confgured
    removed web vitals and boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useSelector, useDispatch } from "react-redux";
+
 function App() {
+  const counter = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    if (counter < 1) {
+      console.log("counter < 1: ", counter < 1);
+      return;
+    }
+    dispatch({ type: "DEC" });
+  };
+
   return (
     <>
-      <div className="App">foo bar koo</div>
+      <div>
+        <h1>counter</h1>
+        <h2>{counter}</h2>
+        <button onClick={decrement}>decrement</button>
+        <button onClick={increment}>increment</button>
+      </div>
     </>
   );
 }
